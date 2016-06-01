e3476602c603685d2a994ed2be2110e36c30cd53
ferretdayo <ferretdayo@hotmail.co.jp>
2016-06-01 12:36:34 +0900

    ファイルのダウンロード機能追加

---
+++ sprint1_9/download.php
@@ -0,0 +1,17 @@
+<?php
+    //ダウンロードさせるファイル名
+    $tmp_file = "./csv/議事録.txt";
+    //保存させる際のファイル名
+    $j_file = "議事録.txt";
+    //日本語対応
+    $tmp_file = mb_convert_encoding($tmp_file, "SJIS", "AUTO");
+    //日本語対応
+    $j_file = mb_convert_encoding($j_file, "SJIS", "AUTO");
+    // ヘッダ
+    header("Content-Type: application/octet-stream");
+    // ダイアログボックスに表示するファイル名
+    header("Content-Disposition: attachment; filename=$j_file");
+    // 対象ファイルを出力する。
+    readfile($tmp_file);
+    exit;
+?>
