8b8cfdd76b09fe483e1d69b5736abb8841b84399
ferretdayo <ferretdayo@hotmail.co.jp>
2016-06-17 18:58:38 +0900

    Revert "3名選んでいるかのテストコードを書いた. set_item.jsにdataが定義されていなかったのでそれを定義するように変更"
    This reverts commit 565e1461ab4395d294b0c43d8911a92f9f246bea.

---
+++ spec/LogicTest.js
@@ -0,0 +1,21 @@
+describe("TestSample> ", function(){
+	describe("Logic> ", function(){
+		it("multiNumber", function(){
+			var target = new Logic();
+			var num = 3;
+			var expected = 9;
+
+			var result = target.squaredNumber(num);
+
+			expect(expected).toEqual(result);
+		})
+	});
+});
+
+describe('Hello Test', function(){
+    it('Test', function(){
+        var targetText = 'ふがふが'; // ここを変更
+        var expectText = 'ふがふが';
+        expect(expectText).toBe(targetText);
+    });
+});
