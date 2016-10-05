cb32c2217988f479022f2994c5bea0c9671369f2
Ikuzus-k <ae14suzuki@g.kumamoto-nct.ac.jp>
2016-10-05 17:57:33 +0900

    test codeの追加

--- vote_app/js/set_item.js
+++ vote_app/js/set_item.js
@@ -1,37 +1,27 @@
 //テキストフォームから入力されたデータを取得する
 function set_item(){
     var N = 3;  //投票する人数
-    var temp = {};
+    var candidateId = {};
     var selected_id_json = {};
     var error = "";
     var data = {};
     var checkId = $('#checkvote').val();
+    var count = 0;
 
-    temp = JSON.parse(localStorage.getItem('Candidate_ID'));
+    //現在選択している候補者リストを取得
+    candidateId = JSON.parse(localStorage.getItem('Candidate_ID'));
+
+    //現在チェックしている候補者数のカウント
+    for (key in candidateId) {
+      count++;
+    }
 
     // QRcodeが入力されていない場合、エラー
     if (checkId === "0") {
       console.log("data = null");
-      error = "正しいIDをQRコードから入力してください。";
+      alert("正しいIDをQRコードから入力してください。");
+      return;
     }
-
-    // 候補者が未選択の場合にエラー
-    if (temp === null) {
-      console.log ("temp = null");
-      alert ("候補者を選択してください");
-    }
-
-    var count = 0; //チェックしている候補者数
-    //選択されているオブジェクトの値をselected_idに代入
-    Object.keys(temp).forEach(function(key){
-        if(temp[key] !== null){
-            //3人まではオブジェクトに入れていく
-            if(count < 3){
-                selected_id_json["name_"+(count+1)] = temp[key];
-            }
-            count++;
-        }
-    });
 
     // count数を見て候補者の選択数をチェック
     if(count < 3){
@@ -41,6 +31,7 @@ function set_item(){
         console.log("count > 3");
         error = "候補者を4名以上選んでます。候補者は3名選んでください";
     }
+
     //エラーがあったらalertして終了
     if(error !== ""){
         alert(error);
@@ -54,9 +45,9 @@ function set_item(){
     data['voter_name'] = obj.voter_name.toString();
 
     //voterのデータと投票のデータのマージ
-    var newdata = $.extend(data,selected_id_json);
+    var newdata = $.extend(data,candidateId);
 
-    //LocalStorageに'Vote_Info'の名前で保存
+    //LocalStorageに投票者と候補者リストを'Vote_Info'の名前で保存
     localStorage.setItem('Vote_Info',JSON.stringify(newdata));
 
     var VoteInfo = JSON.parse(localStorage.getItem('Vote_Info'));
