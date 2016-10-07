031dcfbccdcdf7aae04982f47a942d7f99f711f3
Ikuzus-k <ae14suzuki@g.kumamoto-nct.ac.jp>
2016-10-07 15:27:57 +0900

    aggrgate_appの削除とvote_appをpublicに

--- spec/set_item_Test.js
+++ spec/set_item_Test.js
@@ -5,6 +5,7 @@ describe("set_item> ", function(){
 		    var data = {};
 				var voteinfo = {"voter_name": "テスト", "voter_id": "test"};
 				var str_voteinfo = JSON.stringify(voteinfo);
+				localStorage.clear();
 
 			$('body').append("<div id='voterid'></div>");
 			$('body').append("<input type='hidden' name='checkvote' id='checkvote' value='0'>");
@@ -12,6 +13,7 @@ describe("set_item> ", function(){
 
 		})
 
+		//QRコードが入力されていない場合のテストコード。
 		it("non-dataInQRcode", function() {
 			var selected_json = {
 				contender1:"111",
@@ -23,7 +25,9 @@ describe("set_item> ", function(){
 			expect(console.log).toHaveBeenCalledWith("data = null");
 		});
 
+		//候補者が3人未満の場合のテストコード。
 		it("under_3", function(){
+			$('#checkvote').val("1");
 			var selected_json = {
 				contender3:"333",
 				contender5:"555"
@@ -32,7 +36,10 @@ describe("set_item> ", function(){
 			set_item();
 			expect(console.log).toHaveBeenCalledWith("count < 3");
 		});
+
+		//候補者が4人の場合のテストコード。
 		it("greater_3", function(){
+			$('#checkvote').val("1");
 			var selected_json = {
 				contender3:"333",
 				contender4:"444",
